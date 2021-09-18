8e84e95f2e6d66844142fb0802b3add2dac469f7
riri <ghizlane.elatmani.99@gmail.com>
2021-09-18 17:09:11 +0100

    1.1.0 - Add repositories

---
+++ src/main/java/io/javacode/tpspringmvc/TpSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package io.javacode.tpspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringMvcApplication.class, args);
+	}
+
+}
