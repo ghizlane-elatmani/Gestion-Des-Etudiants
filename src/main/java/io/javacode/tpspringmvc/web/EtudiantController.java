4ed81250884756421ce971bfa0aa6739fbc6a43d
riri <ghizlane.elatmani.99@gmail.com>
2021-09-25 10:30:05 +0100

    1.5.1 - Add method index() and annotations

--- src/main/java/io/javacode/tpspringmvc/web/EtudiantController.java
+++ src/main/java/io/javacode/tpspringmvc/web/EtudiantController.java
@@ -1,13 +1,24 @@
 package io.javacode.tpspringmvc.web;
 
 import io.javacode.tpspringmvc.dao.EtudiantRepository;
+import io.javacode.tpspringmvc.entities.Etudiant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping(value = "/etudiant")
 public class EtudiantController {
 
     @Autowired
     private EtudiantRepository etudiantRepository;
+
+    @RequestMapping(value = "/index")
+    public  String Index(){
+        List<Etudiant> etudiantList = etudiantRepository.findAll();
+        return "etudiants";
+    }
 
 }
