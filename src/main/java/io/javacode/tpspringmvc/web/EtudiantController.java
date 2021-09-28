486637fce9f12802411a7377d1c90853b8246ce5
riri <ghizlane.elatmani.99@gmail.com>
2021-09-28 11:27:20 +0100

    1.11.0 - Display photo and now the user can delete student

--- src/main/java/io/javacode/tpspringmvc/web/EtudiantController.java
+++ src/main/java/io/javacode/tpspringmvc/web/EtudiantController.java
@@ -2,20 +2,25 @@ package io.javacode.tpspringmvc.web;
 
 import io.javacode.tpspringmvc.dao.EtudiantRepository;
 import io.javacode.tpspringmvc.entities.Etudiant;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
@@ -61,11 +66,31 @@ public class EtudiantController {
 
         if(!file.isEmpty()){
             etudiant.setPhoto(file.getOriginalFilename());
-            file.transferTo(new File("/home/riri/sco/" + file.getOriginalFilename()));
         }
 
         etudiantRepository.save(etudiant);
+
+        if(!file.isEmpty()){
+            etudiant.setPhoto(file.getOriginalFilename());
+            file.transferTo(new File("/home/riri/sco/" + etudiant.getId()));
+        }
+
         return "redirect:index";
     }
+
+    @RequestMapping(value = "/getPhoto", produces = MediaType.IMAGE_JPEG_VALUE)
+    @ResponseBody
+    public byte[] getPhoto(Long id) throws Exception {
+        File f = new File("/home/riri/sco/"+id);
+        return IOUtils.toByteArray(new FileInputStream(f));
+
+    }
+
+    @RequestMapping(value = "/supprimer")
+    public String supprimer(Long id){
+        etudiantRepository.deleteById(id);
+        return "redirect:index";
+    }
+
 
 }
